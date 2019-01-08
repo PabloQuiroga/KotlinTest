a2f3f84da0758eec81a19825c115c350f7f4a84a
PabloDQuiroga <jtdoe78@gmail.com>
2019-01-08 12:33:07 -0300

    hola mundo this year

--- src/Inicio.kt
+++ src/Inicio.kt
@@ -1,5 +1,13 @@
 fun main(args: Array<String>){ //punto de entrada de la app
 
-    print("hola mundo")
+    val mensaje: String = "Hola mundo"
+    var anio : String?
+
+    println("Indique el año en curso:")
+    anio = readLine()
+
+    var mensaje_completo: String = mensaje +" "+ anio.toString()
+
+    println(mensaje_completo)
 
 }
